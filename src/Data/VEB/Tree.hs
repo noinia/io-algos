5ce12a6eb9d3ee8d0182fef72c2d1186d3af976c
Frank Staals <frank@fstaals.net>
2021-03-27 11:41:58 +0100

    more work on the embeddings

--- src/Data/VEB/Tree.hs
+++ src/Data/VEB/Tree.hs
@@ -13,10 +13,11 @@ module Data.VEB.Tree where
 
 import           Control.DeepSeq
 import           Control.Exception (assert)
+import           Control.Monad.Identity (Identity(..))
 import           Control.Monad.State.Strict
 import           Data.Bifoldable
 import           Data.Bifunctor
-import           Data.BinaryTree.LeafTree.Complete (split, lg, pow, div2, size, fullTree)
+import           Data.BinaryTree.LeafTree.Complete (split, lg, pow, div2, fullTree)
 import qualified Data.BinaryTree.LeafTree.Complete as Complete
 import           Data.BinaryTree.LeafTree.Core ( Height
                                                , foldTree, traversePrefix
@@ -74,6 +75,8 @@ bottomUp mkLeaf mkNode = go 1 . baseCase . fmap mkLeaf . toNonEmpty
 data Two a = Two !a !a
   deriving (Show,Eq,Functor,Traversable,Foldable)
 
+instance Foldable1 Two
+
 -- pre: even number of elements
 pairUp :: Foldable f => f a -> [Two a]
 pairUp = go . F.toList
@@ -94,24 +97,12 @@ chunksOf k = go . F.toList
 
 type BinaryHeight = Height
 
-
-
 -- | A VEBTree, the children of an internal node are stored in an 'f
 -- :: Type -> Type -> Type'.
 --
-data VEBTree f k v = Leaf !v
+data VEBTree f k v = Leaf {-#UNPACK #-} !k !v
                    | Node {-#UNPACK #-} !BinaryHeight -- ^ binary height of the children
                                         !(f k (Two (VEBTree f k v))) -- ^ Children
-
-
-
-
-
-
-
-
-
-
 
 deriving instance (Show k, Show v, forall a b. (Show a, Show b) => Show (f a b)
                   ) => Show (VEBTree f k v)
@@ -121,53 +112,108 @@ deriving instance (Eq k, Eq v, forall a b. (Eq a, Eq b) => Eq (f a b)
 
 instance Functor (f k) => Functor (VEBTree f k) where
   fmap f = \case
-    Leaf x     -> Leaf $ f x
+    Leaf k x   -> Leaf k $ f x
     Node h chs -> Node h $ fmap (fmap (fmap f)) chs
 
 instance Foldable (f k) => Foldable (VEBTree f k) where
   foldMap f = \case
-    Leaf x     -> f x
+    Leaf _ x   -> f x
     Node h chs -> foldMap (foldMap (foldMap f)) chs
+instance Foldable1 (f k) => Foldable1 (VEBTree f k)
 
 instance Traversable (f k) => Traversable (VEBTree f k) where
   traverse f = \case
-    Leaf x     -> Leaf <$> f x
+    Leaf k x   -> Leaf k <$> f x
     Node h chs -> Node h <$> traverse (traverse (traverse f)) chs
 
 
 instance Bifunctor f => Bifunctor (VEBTree f) where
   bimap f g = \case
-    Leaf x     -> Leaf $ g x
+    Leaf k x   -> Leaf (f k) (g x)
     Node h chs -> Node h $ bimap f (fmap (bimap f g)) chs
 
-instance Traversable (f k) => FunctorWithIndex     Index (VEBTree f k)
-instance Traversable (f k) => FoldableWithIndex    Index (VEBTree f k)
-instance Traversable (f k) => TraversableWithIndex Index (VEBTree f k) where
-  itraverse f s = snd $ runIndexing (traverse (\a -> Indexing (\i -> i `seq` (i + 1, f i a))) s) 0
-  {-# INLINE itraverse #-}
+-- instance Traversable (f k) => FunctorWithIndex     k (VEBTree f k)
+-- instance Traversable (f k) => FoldableWithIndex    k (VEBTree f k)
+-- instance Traversable (f k) => TraversableWithIndex k (VEBTree f k) where
+--   itraverse f s =
+
+--     snd $ runIndexing (traverse (\a -> Indexing (\i -> i `seq` (i + 1, f i a))) s) 0
+--   {-# INLINE itraverse #-}
+
+
+-- instance Traversable (f k) => TraversableWithIndex Index (VEBTree f k) where
+--   itraverse f s = snd $ runIndexing (traverse (\a -> Indexing (\i -> i `seq` (i + 1, f i a))) s) 0
+--   {-# INLINE itraverse #-}
+
+
+
+-- biitraverse     :: (Applicative g, Traversable (f k))
+--                 => (Index -> k -> v -> g b)
+--                 -> VEBTree f k v
+--                 -> g (VEBTree f k b)
+-- biitraverse f s = snd $ runBiIndexing (traverse (\a -> BiIndexing (\i k -> i `seq` (i + 1, f i k a))) s) 0
+-- {-# INLINE biitraverse #-}
+
+
+
 
 
 
 instance Bifoldable f => Bifoldable (VEBTree f) where
   bifoldMap f g = \case
-    Leaf x     -> g x
+    Leaf k x   -> f k <> g x
     Node h chs -> bifoldMap f (foldMap (bifoldMap f g)) chs
 
 instance Bifoldable1 f => Bifoldable1 (VEBTree f)
 
 instance Bitraversable f => Bitraversable (VEBTree f) where
   bitraverse f g = \case
-    Leaf x     -> Leaf <$> g x
+    Leaf k x   -> Leaf <$> f k <*> g x
     Node h chs -> Node h <$> bitraverse f (traverse (bitraverse f g)) chs
 
 
 foldVEB           :: Bifunctor f
-                  => (v -> b) -> (Height -> f k (Two b) -> b) -> VEBTree f k v -> b
+                  => (k -> v -> b) -> (Height -> f k (Two b) -> b) -> VEBTree f k v -> b
 foldVEB leaf node = go
   where
     go = \case
-      Leaf v     -> leaf v
+      Leaf k v   -> leaf k v
       Node h chs -> node h $ second (fmap $ foldVEB leaf node) chs
+
+
+-- | Get a list of bottom trees
+bottoms :: Foldable (f k) => VEBTree f k v -> [VEBTree f k v]
+bottoms = \case
+  Leaf _ _   -> []
+  Node _ chs -> concatMap F.toList $ F.toList chs
+
+
+
+-- | Returns the keys in ascending order
+keys :: (Foldable1 (f k), Bifunctor f) => VEBTree f k v -> NonEmpty k
+keys = fmap fst . toAscList
+
+-- | Returns the elements, in order of ascending key value
+elems :: (Foldable1 (f k), Bifunctor f) => VEBTree f k v -> NonEmpty v
+elems = fmap snd . toAscList
+
+
+
+toAscList :: (Foldable1 (f k), Bifunctor f) => VEBTree f k v -> NonEmpty (k,v)
+toAscList = foldVEB (curry singleton) (\_ chs -> foldMap1 (\(Two l r) -> l <> r) chs)
+  -- FIXME: make this run in linear time, which I don't think it does at the moment
+
+
+
+
+
+
+
+
+
+
+
+
 
 ----------------------------------------
 
@@ -186,24 +232,26 @@ instance Bitraversable NList where
 
 ----------------------------------------
 
-
-
-
-
 fromAscList :: Foldable1 f => f (k,v) -> VEBTree NList k (k,v)
 fromAscList = build fst
 
 
 -- | Builds a VEBTree, from v's in increasing order
 build   :: Foldable1 f => (v -> k) -> f v -> VEBTree NList k v
-build f = snd . bottomUp (\v -> (f v, Leaf v))
-                          (\h n chs -> Node h <$> buildHelper chs)
+build f = snd . bottomUp (mkLeaf f)
+                         (\h n chs -> Node h <$> buildHelper chs)
+
+
+mkLeaf f v = let k = f v in (k, Leaf k v)
+
 
 buildHelper     :: Foldable f => f (a, b) -> (a, NList a (Two b))
 buildHelper chs = case NonEmpty.nonEmpty $ pairUp' chs of
                     Nothing -> error "too few children; need at least 2"
                     Just xs -> let m = fst $ NonEmpty.last xs
                                in (m,NList . fmap snd $ xs)
+
+
 
 pairUp' :: Foldable f => f (k, b) -> [(k, (k, Two b))]
 pairUp' = fmap (\(Two (k,l) (m,r)) -> (m, (k, Two l r))) .  pairUp
@@ -215,9 +263,12 @@ pairUp' = fmap (\(Two (k,l) (m,r)) -> (m, (k, Two l r))) .  pairUp
 
 
 -- | Convert the VEBTree into a normal (complete) binary leaf tree
-toTree :: VEBTree NList k v -> Tree.Tree k v
-toTree = foldVEB Tree.Leaf $ \_ (NList chs) -> replaceLeaf (Complete.fromAscList2 chs)
+toTree      :: (v -> k) -> VEBTree NList k v -> Tree.Tree k (k,v)
+toTree getK = foldVEB leaf node
   where
+    leaf k v = Tree.Leaf (k,v)
+    node _ (NList chs) = replaceLeaf (Complete.fromAscList2 chs)
+
     replaceLeaf = foldTree (\(k,Two l r) -> Tree.Node l k r) Tree.Node
 
 
@@ -238,17 +289,36 @@ toTree = foldVEB Tree.Leaf $ \_ (NList chs) -> replaceLeaf (Complete.fromAscList
 
 newtype TopVeb k v = TopVeb (VEBTree TopVeb k v)
   deriving (Show,Eq,Functor,Foldable,Traversable,Bifunctor,Bifoldable
-           ,FunctorWithIndex Index,FoldableWithIndex Index
+           ,Foldable1
+           -- ,FunctorWithIndex Index,FoldableWithIndex Index
            )
 
-instance TraversableWithIndex Index (TopVeb k) where
-  itraverse f (TopVeb t) = TopVeb <$> itraverse f t
+toVEBTree (TopVeb t) = t
+
+
+
+-- instance TraversableWithIndex Index (TopVeb k) where
+--   itraverse f (TopVeb t) = TopVeb <$> itraverse f t
 
 instance Bitraversable TopVeb where
   bitraverse f g (TopVeb t) = TopVeb <$> bitraverse f g t
 
-pattern Leaf' x = TopVeb (Leaf x)
+pattern Leaf' k x = TopVeb (Leaf k x)
 pattern Node' h chs = TopVeb (Node h chs)
+
+
+
+-- | Convert the VEBTree into a normal (complete) binary leaf tree
+toTree' :: VEBTree TopVeb k v -> Tree.Tree k (k,v)
+toTree' = foldVEB leaf node
+  where
+    leaf k v = Tree.Leaf (k,v)
+    topLeaf k (Two l r) = Tree.Node l k r
+    node _ (TopVeb chs) = foldVEB topLeaf node chs
+
+
+
+
 
 
 fromAscKeys  :: (Foldable1 f, Functor f) => f k -> TopVeb k k
@@ -259,7 +329,7 @@ fromAscList' = build' fst
 
 -- | Builds a VEBTree, from v's in increasing order
 build'   :: Foldable1 f => (v -> k) -> f v -> TopVeb k v
-build' f = TopVeb . snd . bottomUp (\v -> (f v, Leaf v))
+build' f = TopVeb . snd . bottomUp (mkLeaf f)
                                    (\h n chs -> Node h <$> buildHelper' chs)
 
 buildHelper'     :: Foldable f => f (a, b) -> (a, TopVeb a (Two b))
@@ -282,15 +352,123 @@ test = second snd . fromAscList' . NonEmpty.fromList . map (\x -> (x,x))
 -- withTopTrees' :: VEBTree NList k v -> TopVeb k v
 -- withTopTrees' = foldVEB Leaf' $ \h (NList chs) -> Node' h (second snd . fromAscList' $ chs)
 
+size :: TopVeb k v -> Size
+size = \case
+  Leaf' _ _   -> 1
+  Node' h top -> Complete.size h + size top
+
+
+type IBuild k v = Index -> NonEmpty (Embedded.Node k (k,v))
+
+
+printEmbedding :: (Show k, Show v) => TopVeb k v -> IO ()
+printEmbedding = mapM_ print . zip [0..] . F.toList . embed 0
+
+loops = filter (\(i,t) -> case t of
+                           Embedded.Leaf  _    -> False
+                           Embedded.Node l _ r -> i == l || i == r
+               )
+
+printLoops :: (Show k, Show v) => TopVeb k v -> IO ()
+printLoops = mapM_ print . loops . zip [0..] . F.toList . embed 0
+
+
+
+printTopVEB            :: (Show k, Show v) => TopVeb k v -> String
+printTopVEB (TopVeb t) = case t of
+    Leaf _ _   -> show t <> "\n"
+    Node h chs -> mconcat [ "Node \n"
+                                   , "top: ("
+                                   , printTopVEB . second (const ()) $ chs
+                                   , ")\n"
+                                   , "bottoms:\n"
+                                   , concatMap (printTopVEB . TopVeb) $ bottoms t
+                                   , "\n"
+                                   ]
+
+
+
+-- splitTopBottom :: VEBTree TopVeb k v -> [(k,Two (VEBTree TopVeb k v))]
+-- splitTopBottom = \case
+--   Leaf _ _            -> []
+--   Node _ (TopVeb chs) -> F.toList $ toAscList chs
+
 
 
 
 
 embed              :: Index -- ^ first free index we can assign to
                    -> TopVeb k v
-                   -> NonEmpty (Embedded.Node k v)
-embed s (TopVeb t) = foldVEB (singleton . Embedded.Leaf) (\h chs -> undefined) t
+                   -> NonEmpty (Embedded.Node k (k,v))
+embed s (TopVeb t) = foldVEB leaf node t s
+  where
+    leaf        :: k -> v -> IBuild k v
+    leaf k v _i = singleton $ Embedded.Leaf (k,v)
 
+    node         :: Height -> TopVeb k (Two (IBuild k v)) -> IBuild k v
+    node h top i = let top'  = imap' (f (i + size top) (Complete.size h)) top
+                       top'' = second (fmap fst) top'
+                   in embedTop top'' i <> foldMap1 (foldMap1 snd) top'
+
+    -- computes the indices corresponding to the trees l and r
+    f s' sb i (Two l r) = let li = s' + i*2*sb
+                              ri = li + sb
+                          in Two (li, l li) (ri, r ri)
+
+    embedTop            :: TopVeb k (Two Index) -> IBuild k v
+    embedTop (TopVeb t) = foldVEB topLeaf node t
+
+    topLeaf                :: k -> Two Index -> IBuild k v
+    topLeaf k (Two l r) _i = singleton $ Embedded.Node l k r
+
+
+
+-- embed s = \case
+--     Leaf' k v   -> singleton $ Embedded.Leaf (k,v)
+--     Node' h top -> let top'  = ibimap (f (s + size top) (Complete.size h)) top
+--                        top'' = second (fmap fst) top'
+--                    in embedTop s top'' <> foldMap1 (foldMap1 snd) top'
+--   where
+--     -- computes the indices corresponding to the trees l and r
+--     f s' sb i (Two l r) = let li = s' + i*2*sb
+--                               ri = li + sb
+--                           in Two (li,embed' li l) (ri,embed' ri r)
+
+
+-- embed'   :: Index -> VEBTree TopVeb k v -> NonEmpty (Embedded.Node k (k,v))
+-- embed' s = embed s . TopVeb
+
+
+
+-- embedTop       :: Index
+--                -> TopVeb k (Two Index)
+--                -> NonEmpty (Embedded.Node k v)
+-- embedTop s =
+
+
+--   \case
+--   Leaf' k (Two l r) -> singleton $ Embedded.Node l k r
+--   Node' _ chs       -> fmap cleanLeaf $ embed' chs
+
+
+
+
+
+
+
+
+cleanLeaf :: Embedded.Node k (a, Embedded.Node k v) -> Embedded.Node k v
+cleanLeaf = \case
+  Embedded.Leaf (k,lf) -> lf
+  Embedded.Node l k r  -> Embedded.Node l k r
+
+  -- let top' = imap (\i (k, Two l r)) top
+
+
+
+  -- foldVEB (singleton . Embedded.Leaf) node t
+  -- where
+  --   node h top = embed s $ top <>
 
 --------------------------------------------------------------------------------
 -- * Helper functions
@@ -300,6 +478,21 @@ singleton x = x :| []
 
 --------------------------------------------------------------------------------
 -- Directly taken from indexed-traversables but using 'Index' rather than 'Int'
+
+
+imap'   :: Traversable (t k)
+        => (Index -> v -> v')
+        -> t k v
+        -> t k v'
+imap' f = runIdentity . itraverse' (\i v -> Identity $ f i v)
+{-# INLINE imap' #-}
+
+itraverse'     :: (Traversable (t k), Applicative f)
+               => (Index -> v -> f v')
+               -> t k v
+               -> f (t k v')
+itraverse' f s = snd $ runIndexing (traverse (\a -> Indexing (\i -> i `seq` (i + 1, f i a))) s) 0
+{-# INLINE itraverse' #-}
 
 -- | 'Applicative' composition of
 -- @'Control.Monad.Trans.State.Lazy.State' 'Index'@ with a 'Functor',
